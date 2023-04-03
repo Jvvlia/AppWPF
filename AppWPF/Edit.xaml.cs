a36af5dafc21a879bb70d468964b1ca13ce63cc5
Jvvlia <120254852+Jvvlia@users.noreply.github.com>
2023-04-03 23:38:37 +0200

    Add new files

---
+++ AppWPF/Edit.xaml.cs
@@ -0,0 +1,62 @@
+﻿using AppCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AppWPF
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Edit.xaml
+    /// </summary>
+    public partial class Edit : Window
+    {
+        Customer customer;
+        public Edit()
+        {
+            InitializeComponent();
+        }
+
+        public Edit(Customer c) :this()
+        {
+            this.customer = c;
+            TxtName.Text = c.Name;
+            TxtAddress.Text = c.Address;
+            TxtVat.Text = c.VATID;
+            TxtDate.Text = $"{c.CreationDate:dd/MM/yyyy}";
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        private void BtnOk_Click(object sender, RoutedEventArgs e)
+        {
+            bool? res = false;
+            if(!string.IsNullOrEmpty(TxtName.Text) && !string.IsNullOrEmpty(TxtAddress.Text) &&
+                !string.IsNullOrEmpty(TxtDate.Text) && !string.IsNullOrEmpty(TxtVat.Text))
+            {
+                customer.Name = TxtName.Text;
+                customer.Address = TxtAddress.Text;
+                customer.VATID = TxtVat.Text;
+                if (DateTime.TryParseExact(TxtDate.Text, new string[] { "dd-MM-yyyy", "dd/MM/yyyy" },
+                   null, System.Globalization.DateTimeStyles.None, out DateTime date))
+                {
+                    customer.CreationDate = date;
+                }
+                res = true;
+            }
+            DialogResult = res;
+        }
+    }
+}
---
+++ AppWPF/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AppCode;
+
+namespace AppWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Customers cust;
+        public MainWindow()
+        {
+            string fname = "customers.xml";
+            cust = Customers.DeserializeXml(fname);
+            InitializeComponent();
+            if(cust!=null )
+            {
+                LstCustomers.ItemsSource = new ObservableCollection<Customer>(cust.CustomersList);
+                
+            }
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if(cust!= null && LstCustomers.SelectedIndex >-1)
+            {
+                Customer c = LstCustomers.SelectedItem as Customer;
+                if(c!=null)
+                {
+                    cust.RemoveCustomer(c);
+                    LstCustomers.ItemsSource = new ObservableCollection<Customer>(cust.CustomersList);
+                }
+            }
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            TxtEdit.Text = LstCustomers.SelectedItem.ToString();
+            if (cust!=null)
+            {
+                Customer c = LstCustomers.SelectedItem as Customer;
+                Customer clone = c.Clone() as Customer;
+                Edit edit = new(c);
+                bool? result = edit.ShowDialog();
+                if(result==true)
+                {
+                    c.Name = clone.Name;
+                    c.Address = clone.Address;
+                    c.CreationDate = clone.CreationDate;
+                    c.VATID= clone.VATID;
+                }
+            }
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if(cust!=null)
+            {
+                Customer c = new Customer();
+                Edit edit = new Edit(c);
+                bool? result = edit.ShowDialog();
+                if(result == true) 
+                { 
+                    cust.AddCustomer(c);
+                    LstCustomers.ItemsSource = new ObservableCollection<Customer>(cust.CustomersList);
+                }
+            }
+        }
+    }
+}
